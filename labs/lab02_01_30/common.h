9a24b69ff4c089f458f62ef6f3319c98afc5fc93
Jose Perales <joseperales@Joses-MacBook-Pro.local>
2020-01-23 11:10:59 -0600

    Declaring function in leds.h

--- labs/lab02_01_30/common.h
+++ labs/lab02_01_30/common.h
@@ -1,6 +1,7 @@
 #ifndef COMMON_H_
 #define COMMOM_H_
 
+
 #ifndef F_CPU
 #define F_CPU
 #endif //for the F_CPU
@@ -9,7 +10,6 @@
 #include <inttypes.h>
 
 //Adding definitions for each pin.
-
 #define PIN0 0
 #define PIN1 1
 #define PIN2 2
@@ -25,12 +25,14 @@
 #define CONFIG_OUTPUT(port, pin) (SET_BIT(port,pin))
 #define CONFIG_INPUT(port,pin)(CLEAR_BIT(port, pin))
 
+
 //Macros to remember if 1 in a DDR register means if its an output or input
 #define CONFIG_OUTPUT(port, pin) (SET_BIT(port, pin))
 #define CONFIG_INPUT(port, pin) (CLEAR_BIT(port, pin))
 
+
 //Definding a user-defined type called IO_struct which
-//contains 5 cariables.
+//contains 5 variables.
 // & is used to the the address of a variable.
 // * is a type of specifier in a variable declaration.
 typedef  struct {
@@ -39,6 +41,24 @@ typedef  struct {
     uint8_t pin;
     volatile uint8_t * portin;
 };IO_struct;
+
+
+/*
+ * turn on specified on board LED. indicate if inverted (0 turns led on)
+ */
+void led_on(IO_struct * color, int inverted);
+
+/*
+ * turn off specified on board LED. indicate if inverted (1 turns led off)
+ */
+void led_off(IO_struct * color, int inverted);
+
+/*
+ * toggle the led between on and off
+ */
+void led_toggle(IO_struct * color);
+
+
 
 
 
