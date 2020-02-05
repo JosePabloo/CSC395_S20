8be4d70f776fce474dc035f5b9c2d9dbfe0b0db7
Jose Perales <joseperales@Joses-MacBook-Pro.local>
2020-02-05 15:02:44 -0600

    new Buttons.h file

--- labs/lab03_02_06/buttons.h
+++ labs/lab03_02_06/buttons.h
@@ -9,13 +9,29 @@
 #define BUTTONA 3
 #define BUTTONC 0
 
+#define PCINT_BUTTONA PCINT3
+#define PCINT_BUTTONC PCINT0
+
+typedef struct {
+    uint8_t pcint;
+    uint8_t mask;
+    uint8_t enabled;
+    unit8_t prev_state;
+    void (*release_fn) (void);
+    void (*press_fn) (void);
+}; INTERRUPT_struct;
 // These must be defined using initialize_button()
-IO_struct _buttonA;
-IO_struct _buttonC;
+extern IO_struct _buttonA;
+extern IO_struct _buttonC;
+
+extern  INTERRUPT_struct _interruptA;
+extern  INTERRUPT_struct _interruptC;
+
+void enable_pcint(INTERRUPT_struct *state);
 
 
-void initialize_button(int button);
+void setup_button_action(INTERRUPT_struct *state, int release, void(*callback)(void));
 
-int is_button_pressed(IO_struct * button);
+void initialize_button(IO_struct * button);
 
 #endif
