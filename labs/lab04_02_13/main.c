edc93ae806d1b41b25788fd72500e47792c065a7
Jose Perales <joseperales@Joses-MacBook-Pro.local>
2020-02-20 11:50:28 -0600

    putting back code for errors

--- labs/lab04_02_13/main.c
+++ labs/lab04_02_13/main.c
@@ -10,6 +10,20 @@
 #define ZERO 0
 
 
+enum buttonA_states {
+    A_Starting, A_FirstPress, A_SecondPress, A_ThirdPress
+} ButtonA_State;
+enum buttonC_states {
+    C_Starting, C_FirstPress, C_SecondPress, C_ThirdPress
+} ButtonC_State;
+
+//Adding the timers for the different states.
+uint8_t yellowTimer = 0;
+uint8_t greenTimer = 0;
+uint32_t mainTimer = 0;
+
+volatile uint8_t button_a_pressed_counter = 0;
+volatile uint8_t button_c_pressed_counter = 0;
 volatile uint32_t ms_ticks = 0;
 
 /***************************************************************************
@@ -21,22 +35,36 @@ void set_up_timmer() {
      * CTC = 0100
      * 01 = TCCR3B
      * 00 = TCCR3A
+     *
+     * ********
+     *
+     * Set up wavefrom to CTC. (Clear Timer on Compare Match)
+     * CTC = 010
+     * 01 = TCCR3B
+     *
      */
     TCCR3B |= (1 << WGM32);
     TCCR0A |= (1 << WGM01);
+
     /**
      * Setting up the Clock_select_bits to set up the prescaler.
      * Set CS32 bits for 256 prescaler:
+     * **************
+     *  Setting up the Clock_select_bits to set up the prescaler.
+     * Set CS01 bits for 8 prescaler:
      */
     TCCR3B |= (1 << CS32);
-    TCCR3B |= (1 << CS01);
+    TCCR0A |= (1 << CS01);
+
+
+
     //set match to achive 250 ms periods aka 4 hz.
     OCR3A = 15625;
     OCR0A = 2000;
 
     //enable the timer interrupt.
     TIMSK3 |= (1 << OCIE3A);
-    TIMSK0 |= (1 << OCR0A);
+    TIMSK0 |= (1 << OCIE0A);
 }
 
 ISR(TIMER3_COMPA_vect){
@@ -64,14 +92,98 @@ void initialize_system(void) {
     external_light_show();
 
     set_up_timmer();
+    // initalize only buttonA and buttonC because they are connected to PCINT
+    // NOTE: button C and the RED led are on the same line.
+    initialize_button(&_buttonA);
+    initialize_button(&_buttonC);
+
+    enable_pcint(&_interruptA);
+    enable_pcint(&_interruptC);
 
 
+    // Setting up the button action. when its pressed or released.
+    // When it's released, called the release funciton.
+    // when released call release function
 
+//    setup_button_action(&_interruptA, 0);
+//    setup_button_action(&_interruptC, 0);
 
+    setup_button_action(&_interruptA, 1, button_a_pressed);
+    setup_button_action(&_interruptC, 1, button_c_pressed);
 }
 
+//This is going to hold the state of the Yellow LED.=
+void Yellow_LED_Cases() {
+    switch (ButtonA_State) {   // Transitions
+        // Initial transition
+        //Stating off with the LED off.
+        //If the Button A is pressed, Move the Flag counter to the first relase.
+        case A_Starting:
+            led_off(&_yellow, 0);
+            if (1 == button_a_pressed_counter) {
+                ButtonA_State = A_FirstPress;
+            }
+            break;
+            //First press is the second  case is turning on the LED.
+        case A_FirstPress:
+            led_on(&_yellow, INVERTED);
+            if (2 == button_a_pressed_counter) {
+                ButtonA_State = A_SecondPress;
+            }
+            break;
+            // Second Press is flashing at .4 hz
+        case A_SecondPress:
+            if (yellowTimer % 1250 == 0) {
+                led_toggle(&_yellow);
+            }
+            if (3 == button_a_pressed_counter) {
+                ButtonA_State = A_Starting;
+            }
+            break;
+        default:
+            ButtonA_State = A_Starting;
+            break;
+    } // Transitions
 
 
+} // State actions
+
+
+
+void Green_LED_Cases() {
+    switch (ButtonC_State) {   // Transitions
+        // Initial transition
+        //Stating off with the LED off.
+        //If the Button A is pressed, Move the Flag counter to the first relase.
+        case C_Starting:
+            led_off(&_green, 1);
+            if (1 == button_c_pressed_counter) {
+                ButtonC_State = C_FirstPress;
+            }
+            break;
+            //First press is the second  case is turning on the LED.
+        case C_FirstPress:
+            led_on(&_green, 0);
+            if (2 == button_c_pressed_counter) {
+                ButtonC_State = C_SecondPress;
+            }
+            break;
+            // Second Press is flashing at .4 hz
+        case C_SecondPress:
+            if (greenTimer % 250 == 0) {
+                led_toggle(&_green);
+            }
+            if (3 == button_c_pressed_counter) {
+                ButtonC_State = C_Starting;
+            }
+            break;
+        default:
+            ButtonC_State = C_Starting;
+            break;
+    } // Transitions
+
+
+} // State actions
 
 
 
@@ -87,13 +199,27 @@ int main(void) {
 
     initialize_system();
 
+    //*************************************************************//
+    //*******         THE CYCLIC CONTROL LOOP            **********//
+    //*************************************************************//
+
+
+//    button_a_pressed_counter = 0;
+//    ButtonA_State = A_Starting;
+//
+//    button_c_pressed_counter = 0;
+//    ButtonC_State = C_Starting;
+
+
     sei();  //calling this from main.
+
+
+
 
     while (1) {
 
         if (ms_ticks % 500 == 0) {
             led_toggle(&_redbb);
-            led_toggle(&_red);
         }
     } // end while(1)
 
