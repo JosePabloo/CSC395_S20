e20cc81744954ceac530db4bbc5de18938a64c0a
Jose Perales <joseperales@Joses-MacBook-Pro.local>
2020-03-02 23:33:14 -0600

    adding extern task and task structure and defining task

--- labs/lab06_02_27/task.h
+++ labs/lab06_02_27/task.h
@@ -4,4 +4,24 @@
 #include <util/delay.h>
 #include <inttypes.h>
 
+#define REDTASK 0
+#define YELLOWTASK 1
+#define BUTTONATASK 2
+#define BUTTONCTASK 3
+#define PRINT 4
+
+typedef  struct {
+    uint8_t ready;
+    uint32_t nextRelease;
+    uint32_t period;
+    void (*tick_fn) (void);
+} task;
+
+
+extern task _redTask;
+extern task _yellowTask;
+extern task _buttonATask;
+extern task _buttonCTask;
+extern task _print;
+
 #endif
